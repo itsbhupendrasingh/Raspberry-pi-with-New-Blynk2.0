a340f4a9eb281036a255270bb45b434cc60ddb50
Bhupendra Singh <45242141+itsbhupendrasingh@users.noreply.github.com>
2022-12-09 15:49:24 +0530

    Update LED_Control_New_Blynk.py

--- LED_Control_New_Blynk.py
+++ LED_Control_New_Blynk.py
@@ -1,25 +1,5 @@
 """
-Blynk is a platform with iOS and Android apps to control
-Arduino, Raspberry Pi and the likes over the Internet.
-You can easily build graphic interfaces for all your
-projects by simply dragging and dropping widgets.
-
-  Downloads, docs, tutorials: http://www.blynk.cc
-  Sketch generator:           http://examples.blynk.cc
-  Blynk community:            http://community.blynk.cc
-  Social networks:            http://www.fb.com/blynkapp
-                              http://twitter.com/blynk_app
-
-This example shows how to perform custom actions
-using data from the widget.
-
-In your Blynk App project:
-  Add a Slider widget,
-  bind it to Virtual Pin V3.
-  Run the App (green triangle in the upper right corner)
-
-It will automagically call v3_write_handler.
-In the handler, you can use args[0] to get current slider value.
+This code is Belongs to SME Dehradun. you can mail us schematicslab@gmail.com for any query
 """
 
 import BlynkLib
